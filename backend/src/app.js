20739dda671b81f5b39e3ffb49d79ec10fab7e12
naydin <naydin@natera.com>
2024-12-07 17:08:54 +0300

    create app.js and mongodb connection

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const connectToMongo = require('./db/connection');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    connectToMongo()
+  });
+}
---
+++ backend/src/db/connection.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+
+const mongo_db_password = process.env.MONGO_DB_PASSWORD
+
+
+let url = `mongodb+srv://nilay:${mongo_db_password}@cluster0.6zfqt.mongodb.net/`;
+
+console.log('Connection URL:', url);
+console.log("AAA")
+
+const connectToMongo = () => {
+  mongoose.connect(url);
+
+  const db = mongoose.connection;
+
+  db.once('open', () => {
+    console.log('Database connected to mongodb');
+  });
+
+  db.on('error', (err) => {
+    console.error('Database connection error: ', err);
+  });
+};
+
+module.exports = connectToMongo;
