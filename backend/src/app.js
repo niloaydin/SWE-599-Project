c775458fb53bea49ea1c5d757415b7ee75ad378a
naydin <naydin@natera.com>
2024-12-08 16:36:36 +0300

    add route config to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const connectToMongo = require('./db/connection');
+const discussionRoutes = require('./routes/discussionRoute');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,6 +11,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use("/api/discussion", discussionRoutes);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
