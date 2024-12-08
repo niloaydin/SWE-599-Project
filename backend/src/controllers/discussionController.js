5ab6caab4b97a03a25657f840c840dc0440d8d99
naydin <naydin@natera.com>
2024-12-08 18:04:41 +0300

    create collector controller for discussions

--- backend/src/controllers/discussionController.js
+++ backend/src/controllers/discussionController.js
@@ -1,43 +1,46 @@
-const creatorModel = require('../models/creatorModel');
-const discussionModel = require('../models/discussionModel');
+const CreatorModel = require('../models/creatorModel');
+const DiscussionModel = require('../models/discussionModel');
+const UserLinkModel = require('../models/userLinkModel');
+const CollectorModel = require('../models/collectorModel');
 const { generateRandomString } = require('../utils/discussionUtils');
 
 const createDiscussion = async (req, res) => {
 
-  const {email,title,description,duration} = req.body;
-  try{
+  const { email, title, description, duration } = req.body;
+  try {
 
-  const creator = await creatorModel.create({
-    email: email
-  })
-  await creator.save();
+    let creator = await CreatorModel.findOne({ email: email });
 
-  const startDate = new Date(); 
-  const endDate = new Date(startDate.getTime() + duration * 24 * 60 * 60 * 1000); 
+    if (!creator) {
+      creator = await CreatorModel.create({
+        email: email
+      })
+    }
 
-  const dLink=generateRandomString();
-  const adminLink=generateRandomString();
+    const startDate = new Date();
+    const endDate = new Date(startDate.getTime() + duration * 24 * 60 * 60 * 1000);
 
-  console.log("dLink",dLink);
-  console.log("adminLink",adminLink);
+    const dLink = generateRandomString();
+    const adminLink = generateRandomString();
 
-  const discussion = await discussionModel.create({
-    title: title,
-    description: description,
-    startDate: startDate,
-    endDate: endDate,
-    dLink: dLink,
-    adminLink: adminLink,
-    creatorId: creator._id,
-    isVotingStarted: false
-  })
+    console.log("dLink", dLink);
+    console.log("adminLink", adminLink);
 
-  await discussion.save();
+    const discussion = await DiscussionModel.create({
+      title: title,
+      description: description,
+      startDate: startDate,
+      endDate: endDate,
+      dLink: dLink,
+      adminLink: adminLink,
+      creatorId: creator._id,
+      isVotingStarted: false
+    })
 
-  return res.status(200).json({ message: discussion });
-}catch(error){
-  return res.status(400).json({ message: error.message });
-}
+    return res.status(200).json({ message: discussion });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 const getSingleDiscussion = async (req, res) => {
@@ -46,8 +49,63 @@ const getSingleDiscussion = async (req, res) => {
 };
 
 const createCollectorForDiscussion = async (req, res) => {
-  console.log('createCollectorForDiscussion controller');
-  return res.status(200).json({ message: 'createCollectorForDiscussion controller' });
+  const { discussionLink } = req.params;
+  const { collectorName, emails, type } = req.body;
+
+  try {
+    if (!['general', 'specific'].includes(type)) {
+      return res.status(400).json({ error: 'Invalid collector type' });
+    }
+
+    const discussion = await DiscussionModel.findOne({ dLink: discussionLink });
+
+    if (!discussion) {
+      return res.status(404).json({ error: 'Discussion not found' });
+    }
+
+    const collector = await CollectorModel.create({
+      discussionId: discussion._id,
+      collectorName: collectorName,
+      collectorType: type,
+      listLinks: []
+    })
+
+    let listLinks = [];
+
+    if (type === 'general') {
+      const generalLink = generateRandomString();
+      listLinks.push(generalLink);
+
+      await UserLinkModel.create({
+        collectorId: collector._id,
+        discussionId: discussion._id,
+        linkUUID: generalLink,
+      })
+
+    } else if (type === 'specific' && emails && emails.length > 0) {
+
+      for (const email of emails) {
+        const personalizedLink = generateRandomString();
+
+        await UserLinkModel.create({
+          collectorId: collector._id,
+          discussionId: discussion._id,
+          email: email,
+          linkUUID: personalizedLink
+        })
+
+        listLinks.push(personalizedLink);
+
+      }
+    }
+
+    collector.listLinks = listLinks;
+    await collector.save();
+
+    return res.status(200).json({ message: collector });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 const createDiscussionGeneralLink = async (req, res) => {
